29a39d48fb855e661924beea26db00c10aedbf2f
Muryllo123 <jitave.03@gmail.com>
2022-11-27 21:33:11 -0300

    Versão Completa

---
+++ Inicio/script.js
@@ -0,0 +1,13 @@
+var links = document.getElementById("links")
+
+function showMenu(){
+    links.style.right ="0";
+    links.style.display = "block";
+  
+
+}
+
+function hideMenu(){
+    links.style.right = "-200px";
+    links.style.display = "none";
+}
