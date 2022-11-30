63a3611cdc6fe2f08b29e4a5ad02cf5611aade97
Muryllo123 <jitave.03@gmail.com>
2022-11-30 14:54:22 -0300

    Projeto

--- Inicio/script.js
+++ Inicio/script.js
@@ -11,3 +11,15 @@ function hideMenu(){
     links.style.right = "-200px";
     links.style.display = "none";
 }
+const login = document.getElementById("login")
+const dialog = document.getElementById("logar")
+login.onclick = function (){
+    logar.showModal()
+}
+
+const cadastro = document.getElementById("cadastro")
+const modal = document.getElementById("cadastrar")
+
+cadastro.onclick = function () {
+    modal.showModal ()
+}
