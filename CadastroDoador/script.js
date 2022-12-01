6c804634b5cf5420dd1393e57ae2ff7bf6373388
Muryllo123 <jitave.03@gmail.com>
2022-12-01 00:41:13 -0300

    Projeto

--- CadastroDoador/script.js
+++ CadastroDoador/script.js
@@ -1,6 +1,11 @@
 const concordo = document.getElementById("concordo")
 const dialog = document.querySelector("dialog")
+const buttonClose = document.querySelector("dialog button")
 
 concordo.onclick =function (){
     dialog.showModal()
+}
+
+buttonClose.onclick = function (){
+    dialog.close()
 }
